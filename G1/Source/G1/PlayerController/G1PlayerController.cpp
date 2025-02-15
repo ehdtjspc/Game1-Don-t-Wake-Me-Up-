60d18e1c3794a7b623addf8e327809ec4b0b9240
ehdtjspc <109285423+ehdtjspc@users.noreply.github.com>
2025-02-15 18:18:10 +0900

    2025-02-15 (중간 업데이트)

--- G1/Source/G1/PlayerController/G1PlayerController.cpp
+++ G1/Source/G1/PlayerController/G1PlayerController.cpp
@@ -2,4 +2,63 @@
 
 
 #include "PlayerController/G1PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Kismet/KismetMathLibrary.h"
 
+
+AG1PlayerController::AG1PlayerController(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+}
+
+void AG1PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (auto* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(InputMappingContext, 0);
+	}
+
+}
+
+void AG1PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (auto* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Input_Move);
+		EnhancedInputComponent->BindAction(TurnAction, ETriggerEvent::Triggered, this, &ThisClass::Input_Turn);
+
+	}
+}
+
+
+void AG1PlayerController::Input_Move(const FInputActionValue& InputValue)
+{
+	FVector2D MovementVector = InputValue.Get<FVector2D>();
+
+	if (MovementVector.X != 0)
+	{
+		FRotator Rotator = GetControlRotation();
+		FVector Direction = UKismetMathLibrary::GetForwardVector(FRotator(0, Rotator.Yaw, 0));
+		GetPawn()->AddMovementInput(Direction, MovementVector.X);
+	}
+
+	if (MovementVector.Y != 0)
+	{
+		FRotator Rotator = GetControlRotation();
+		FVector Direction = UKismetMathLibrary::GetRightVector(FRotator(0, Rotator.Yaw, 0));
+		GetPawn()->AddMovementInput(Direction, MovementVector.Y);
+	}
+
+
+}
+
+void AG1PlayerController::Input_Turn(const FInputActionValue& InputValue)
+{
+	float Val = InputValue.Get<float>();
+	AddYawInput(Val);
+}
--- G1/Source/G1/PlayerController/G1PlayerController.h
+++ G1/Source/G1/PlayerController/G1PlayerController.h
@@ -6,12 +6,36 @@
 #include "GameFramework/PlayerController.h"
 #include "G1PlayerController.generated.h"
 
+struct FInputActionValue;
+
 /**
- * 
+ *
  */
 UCLASS()
 class G1_API AG1PlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+	AG1PlayerController(const FObjectInitializer& ObjectInitializer);
+
+private:
+	void Input_Move(const FInputActionValue& InputValue);
+	void Input_Turn(const FInputActionValue& InputValue);
+
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+
+protected:
+	UPROPERTY(EditAnywhere, Category = Input)
+	TObjectPtr<class UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	TObjectPtr<class UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	TObjectPtr<class UInputAction> TurnAction;
 };
