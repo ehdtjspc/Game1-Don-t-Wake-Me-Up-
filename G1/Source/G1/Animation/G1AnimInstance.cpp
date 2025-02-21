ba847544481be64dcc981b15130f8877cf184861
ehdtjspc <109285423+ehdtjspc@users.noreply.github.com>
2025-02-21 20:46:06 +0900

    2025-02-21(애니메이션 c++ 작업)

---
+++ G1/Source/G1/Animation/G1AnimInstance.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/G1AnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Character/Player/G1Player.h"
+
+UG1AnimInstance::UG1AnimInstance(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+}
+
+void UG1AnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Character = Cast<AG1Player>(TryGetPawnOwner());
+
+	if (Character)
+	{
+		MovementComponent = Character->GetCharacterMovement();
+	}
+}
+
+void UG1AnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (Character == nullptr)
+		return;
+
+	if (MovementComponent == nullptr)
+		return;
+
+	Velocity = MovementComponent->Velocity;
+	GroundSpeed = Velocity.Size2D();
+
+	bShouldMove = (GroundSpeed > 3.f && MovementComponent->GetCurrentAcceleration() != FVector::ZeroVector);
+	bIsFalling = MovementComponent->IsFalling();
+
+}
---
+++ G1/Source/G1/Animation/G1AnimInstance.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "G1AnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class G1_API UG1AnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UG1AnimInstance(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<class AG1Player> Character;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<class UCharacterMovementComponent> MovementComponent;
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	FVector Velocity = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadOnly)
+	float GroundSpeed = 0.f;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bShouldMove = false;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsFalling = false;
+
+
+};
