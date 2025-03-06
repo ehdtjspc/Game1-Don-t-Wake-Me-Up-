bd05b0b3688a050f67a8cd354da493a0d9fce295
ehdtjspc <109285423+ehdtjspc@users.noreply.github.com>
2025-03-06 23:40:31 +0900

    2025-03-06(Behavior Tree 제작)

---
+++ G1/Source/G1/AI/G1AIController.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/G1AIController.h"
+#include "Navigation/PathFollowingComponent.h"
+
+AG1AIController::AG1AIController(const FObjectInitializer& ObjectInitializer)
+{
+}
+
+void AG1AIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+}
+
+void AG1AIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	/*FVector Dest = {0,0,0};
+
+	FAIMoveRequest MoveRequest;
+	MoveRequest.SetGoalLocation(Dest);
+	MoveRequest.SetAcceptanceRadius(15.f);
+
+	FNavPathSharedPtr NavPath;
+	MoveTo(MoveRequest,OUT &NavPath);
+
+	if (NavPath.IsValid())
+	{
+		TArray<FNavPathPoint>& PathPoints = NavPath->GetPathPoints();
+		for (const auto& Point : PathPoints)
+		{
+			const FVector& Location = Point.Location;
+			DrawDebugSphere(GetWorld(), Location, 12.f, 12, FColor::Green, false, 10.0f);
+		}
+	} */
+}
+
+void AG1AIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ G1/Source/G1/AI/G1AIController.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "G1AIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class G1_API AG1AIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	AG1AIController(const FObjectInitializer& ObjectInitializer);
+
+	virtual void OnPossess(APawn* InPawn) override;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+};
